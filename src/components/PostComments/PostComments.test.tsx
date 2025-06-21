66add2403277d83ceaaa7676c3716da3ce0523d9
GiovanaMv <saddnez@gmail.com>
2025-06-21 18:59:01 -0300

    exercicio

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,23 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import Post from './index'; // esse é seu componente certo
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe('Teste para o componente PostComments', () => {
+    it('deve inserir dois comentários e exibi-los na tela', () => {
+        render(<Post />);
+
+        const inputComentario = screen.getByTestId('input-comentario') as HTMLTextAreaElement;
+        const botaoEnviar = screen.getByTestId('botao-enviar');
+
+        // Primeiro comentário
+        fireEvent.change(inputComentario, { target: { value: 'Primeiro comentário!' } });
+        fireEvent.click(botaoEnviar);
+
+        // Segundo comentário
+        fireEvent.change(inputComentario, { target: { value: 'Segundo comentário!' } });
+        fireEvent.click(botaoEnviar);
+
+        // Verifica se os dois aparecem
+        expect(screen.getByText('Primeiro comentário!')).toBeInTheDocument();
+        expect(screen.getByText('Segundo comentário!')).toBeInTheDocument();
     });
 });
